18a91c076dbcccbcde015da21a79d1392d07bb80
Trần Đức Trung <38934471+niits@users.noreply.github.com>
2024-09-23 10:06:07 +0700

    Update partitions.py

--- app/bank_data/partitions.py
+++ app/bank_data/partitions.py
@@ -1,3 +1,3 @@
 from dagster import DailyPartitionsDefinition
 
-daily_partitions = DailyPartitionsDefinition(start_date="2024-05-26")
+daily_partitions = DailyPartitionsDefinition(start_date="2024-09-19")
